d538df33643e3289f4366614d55ed6b62b559ed7
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-11 16:24:18 -0400

    Added solution for python

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,17 @@
 def caesar_cipher(string, shift_amount):
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    new_string = ""
+    for ltr in string:
+        if ltr in lowercase:
+            index = lowercase.index(ltr)
+            new_index = (index + shift_amount) % 26
+            new_string += lowercase[new_index]
+        elif ltr in uppercase:
+            index = uppercase.index(ltr)
+            new_index = (index + shift_amount) % 26
+            new_string += uppercase[new_index]
+        else:
+            new_string += ltr
+
+    return new_string
